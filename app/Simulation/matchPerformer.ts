175cfc11c2c3833c908be026728a24f65611c0f4
danzel <dave@ironshod.co.nz>
2016-01-12 19:26:05 +1300

    Add a matchPerformed event to the matchPerformer

--- app/Simulation/matchPerformer.ts
+++ app/Simulation/matchPerformer.ts
@@ -1,4 +1,5 @@
 import Grid = require('./grid');
+import LiteEvent = require('../liteEvent');
 import Matchable = require('./matchable');
 import MatchChecker = require('./matchChecker');
 import Physics = require('./physics');
@@ -6,8 +7,10 @@ import Swap = require('./swap');
 import SwapHandler = require('./swapHandler');
 
 class MatchPerformer {
-	matchChecker: MatchChecker;
-	
+	private matchChecker: MatchChecker;
+
+	matchPerformed = new LiteEvent<Array<Matchable>>();
+
 	constructor(matchChecker: MatchChecker, swapHandler: SwapHandler, physics: Physics) {
 		this.matchChecker = matchChecker;
 
@@ -15,7 +18,7 @@ class MatchPerformer {
 		physics.matchableLanded.on(this.testForMatch.bind(this));
 	}
 
-	onSwapOccurred(swap: Swap) {
+	private onSwapOccurred(swap: Swap) {
 		this.testForMatch(swap.left);
 
 		if (!swap.right.isDisappearing)
@@ -23,26 +26,28 @@ class MatchPerformer {
 	}
 
 	private testForMatch(matchable: Matchable) {
-		
 		var matchDetails = this.matchChecker.testForMatch(matchable);
-		
+
 		if (matchDetails) {
 			this.performMatch(matchable, matchDetails.horizontal, matchDetails.vertical);
 		}
 	}
-	
+
 	private performMatch(matchable: Matchable, horizontal: boolean, vertical: boolean) {
+		let matched = [matchable];
 
 		if (horizontal) {
-			this.matchChecker.scanLeft(matchable, (hit: Matchable) => hit.isDisappearing = true);
-			this.matchChecker.scanRight(matchable, (hit: Matchable) => hit.isDisappearing = true);
+			this.matchChecker.scanLeft(matchable, (hit: Matchable) => { matched.push(hit); hit.isDisappearing = true });
+			this.matchChecker.scanRight(matchable, (hit: Matchable) => { matched.push(hit); hit.isDisappearing = true });
 		}
 		if (vertical) {
-			this.matchChecker.scanUp(matchable, (hit: Matchable) => hit.isDisappearing = true);
-			this.matchChecker.scanDown(matchable, (hit: Matchable) => hit.isDisappearing = true);
+			this.matchChecker.scanUp(matchable, (hit: Matchable) => { matched.push(hit); hit.isDisappearing = true });
+			this.matchChecker.scanDown(matchable, (hit: Matchable) => { matched.push(hit); hit.isDisappearing = true });
 		}
 
 		matchable.isDisappearing = true;
+		
+		this.matchPerformed.trigger(matched);
 	}
 }
 
