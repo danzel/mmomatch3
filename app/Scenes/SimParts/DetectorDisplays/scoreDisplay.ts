23f55288eb7f8a3fd72123bde29ef6bb43a88b69
danzel <dave@ironshod.co.nz>
2016-03-08 22:10:29 +1300

    Have the ScoreDisplay actually update to show the score

--- app/Scenes/SimParts/DetectorDisplays/scoreDisplay.ts
+++ app/Scenes/SimParts/DetectorDisplays/scoreDisplay.ts
@@ -6,10 +6,16 @@ import DetectorDisplay = require('../detectorDisplay');
 class ScoreDisplay extends DetectorDisplay {
 	constructor(private group: Phaser.Group, private detector: ScoreDetector) {
 		super();
-		let text = new Phaser.Text(group.game, 0, 0, "Score Required: " + detector.scoreRequired, {
+		let text = new Phaser.Text(group.game, 0, 0, "Score Required: " + detector.scoreRequiredRemaining + "/" + detector.scoreRequired, {
 			fill: 'white'
 		});
 		this.group.add(text);
+		
+		detector.valueChanged.on(() => {
+			if (!this.disabled) {
+				text.text = "Score Required: " + detector.scoreRequiredRemaining + "/" + detector.scoreRequired;
+			}
+		})
 	}
 }
 
