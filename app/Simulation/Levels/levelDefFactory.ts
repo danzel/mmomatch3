371ed34e5d05e3b1429b55d7e4e4f25c0f383889
David Leaver <danzel@localhost.geek.nz>
2016-03-12 08:37:14 +1300

    Some levels for multiplayer. level 2 a bit easy maybe

--- app/Simulation/Levels/levelDefFactory.ts
+++ app/Simulation/Levels/levelDefFactory.ts
@@ -3,10 +3,23 @@ import LevelDef = require('./levelDef');
 import VictoryType = require('./victoryType');
 
 class LevelDefFactory {
-	
-	
+
+	debugLevel = false;
+
 	getLevel(levelNumber: number): LevelDef {
-		return new LevelDef(levelNumber, 10, 10, [], 8, FailureType.Time, VictoryType.Matches, 10, 31);
+		if (this.debugLevel) {
+			return new LevelDef(levelNumber, 10, 10, [], 8, FailureType.Time, VictoryType.Matches, 10, 31);
+		}
+		
+		if (levelNumber % 3 == 0) {
+			return new LevelDef(levelNumber, 20, 50, [], 8, FailureType.Time, VictoryType.Score, 180, 200 * 1000);
+		}
+		if (levelNumber % 3 == 1) {
+			return new LevelDef(levelNumber, 50, 25, [], 8, FailureType.Time, VictoryType.Score, 180, 200 * 1000);
+		}
+		if (levelNumber % 3 == 2) {
+			return new LevelDef(levelNumber, 30, 30, [], 8, FailureType.Swaps, VictoryType.Score, 100, 60 * 1000);
+		}
 	}
 }
 
