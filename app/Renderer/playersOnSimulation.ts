2aca30296fe80488ae68c51fdf40cc5e0278ab2a
danzel <dave@ironshod.co.nz>
2016-03-22 16:53:51 +1300

    Color the players randomly

--- app/Renderer/playersOnSimulation.ts
+++ app/Renderer/playersOnSimulation.ts
@@ -1,3 +1,6 @@
+/// <reference path="../../typings/mersenne-twister/mersenne-twister.d.ts" />
+import MersenneTwister = require('mersenne-twister');
+
 import MatchableNode = require('./matchableNode');
 import Simulation = require('../Simulation/simulation');
 import Swap = require('../Simulation/swap');
@@ -8,41 +11,51 @@ class PlayerSprite {
 	constructor(parentGroup: Phaser.Group, private playerId: number, x: number, y: number) {
 		this.sprite = parentGroup.game.add.sprite(x, y, 'player', null, parentGroup);
 		this.sprite.anchor.set(0.5);
-		//TODO this.sprite.tint
+
+		//Semi bad random color generator.
+		let mask = 0x55;
+		let tint = 0;
+		let maskNeg = (0xff - mask);
+		let rand = new MersenneTwister(playerId);
+		for (let i = 0; i < 3; i++) {
+			tint <<= 8;
+			tint += (mask + rand.random() * maskNeg) | 0;
+		}
+		this.sprite.tint = tint;
 	}
-	
+
 	notifyPosition(x: number, y: number) {
 		this.sprite.game.add.tween(this.sprite)
-			.to({ x: x, y: y}, 100)
+			.to({ x: x, y: y }, 100, Phaser.Easing.Cubic.In)
 			.start();
 	}
 }
 
 class PlayersOnSimulation {
 	group: Phaser.Group;
-	playerSprites: {[playerId: number]: PlayerSprite } = {};
-	
+	playerSprites: { [playerId: number]: PlayerSprite } = {};
+
 	constructor(simulation: Simulation, simulationGroup: Phaser.Group) {
 		//We are a child of simGroup so we get the scaling and stuff
 		this.group = simulationGroup.game.add.group(simulationGroup);
-		
+
 		simulation.swapHandler.swapStarted.on((swap) => this.swapStarted(swap));
 	}
-	
+
 	private swapStarted(swap: Swap) {
 		//TODO: Don't do anything if this is us doing the swap
-		
+
 		//Sorta stolen from MatchableNode.updatePosition
 		let x = (swap.left.x + swap.right.x) / 2 * MatchableNode.PositionScalar + (MatchableNode.PositionScalar / 2);
 		let y = - (swap.left.y + swap.right.y) / 2 * MatchableNode.PositionScalar - (MatchableNode.PositionScalar / 2);
-		
-		
+
+
 		if (this.playerSprites[swap.playerId]) {
-			 this.playerSprites[swap.playerId].notifyPosition(x, y);
-			
- 		} else {
-			 this.playerSprites[swap.playerId] = new PlayerSprite(this.group, swap.playerId, x, y);
-		 }
+			this.playerSprites[swap.playerId].notifyPosition(x, y);
+
+		} else {
+			this.playerSprites[swap.playerId] = new PlayerSprite(this.group, swap.playerId, x, y);
+		}
 	}
 }
 
