00436428ec27f5c58f957741156ab0400eb348b7
danzel <dave@ironshod.co.nz>
2016-03-29 22:11:57 +1300

    Load requireMatchInCellTracker from the victoryValue

--- app/Server/defaultLevelAndSimulationProvider.ts
+++ app/Server/defaultLevelAndSimulationProvider.ts
@@ -4,8 +4,10 @@ import LevelDef = require('../Simulation/Levels/levelDef');
 import LevelDefFactory = require('../Simulation/Levels/levelDefFactory');
 import MatchableFactory = require('../Simulation/matchableFactory');
 import RandomGenerator = require('../Simulation/randomGenerator');
+import RequireMatch = require('../Simulation/requireMatch');
 import Simulation = require('../Simulation/simulation');
 import SpawningSpawnManager = require('../Simulation/spawningSpawnManager');
+import VictoryType = require('../Simulation/Levels/victoryType');
 
 class DefaultLevelAndSimulationProvider implements LevelAndSimulationProvider {
 	constructor(private levelDefFactory: LevelDefFactory) {
@@ -18,6 +20,13 @@ class DefaultLevelAndSimulationProvider implements LevelAndSimulationProvider {
 		let matchableFactory = new MatchableFactory();
 		let spawnManager = new SpawningSpawnManager(grid, matchableFactory, new RandomGenerator(), level.colorCount);
 		let simulation = new Simulation(grid, spawnManager, matchableFactory, 60);
+
+		if (level.victoryType == VictoryType.RequireMatch) {
+			let requireMatches = <Array<{ x: number, y: number; amount: number }>>level.victoryValue;
+			requireMatches.forEach(req => {
+				simulation.requireMatchInCellTracker.requirements.push(new RequireMatch(req.x, req.y, req.amount));
+			})
+		}
 
 		return { level: level, simulation: simulation };
 	}
--- test/util/testLASProvider.ts
+++ test/util/testLASProvider.ts
@@ -1,12 +1,22 @@
 import LevelAndSimulationProvider = require('../../app/Server/levelAndSimulationProvider');
 import LevelDef = require('../../app/Simulation/Levels/levelDef');
+import RequireMatch = require('../../app/Simulation/requireMatch');
 import Simulation = require('../../app/Simulation/simulation');
+import VictoryType = require('../../app/Simulation/Levels/victoryType');
 
 class TestLASProvider implements LevelAndSimulationProvider {
 	constructor(private level: LevelDef, private simulation: Simulation) {
 	}
 
 	loadLevel(levelNumber: number): { level: LevelDef, simulation: Simulation } {
+
+		if (this.level.victoryType == VictoryType.RequireMatch) {
+			let requireMatches = <Array<{ x: number, y: number; amount: number }>>this.level.victoryValue;
+			requireMatches.forEach(req => {
+				this.simulation.requireMatchInCellTracker.requirements.push(new RequireMatch(req.x, req.y, req.amount));
+			})
+		}
+
 		return { level: this.level, simulation: this.simulation };
 	}
 };
