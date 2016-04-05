53a77277b9c9e6875adf125802d2d12e1e061214
danzel <dave@ironshod.co.nz>
2016-04-05 21:12:47 +1200

    Make levelDetailsOverlay look nicer

--- app/Scenes/SimParts/levelDetailsOverlay.ts
+++ app/Scenes/SimParts/levelDetailsOverlay.ts
@@ -4,23 +4,47 @@ import LiteEvent = require('../../liteEvent');
 import TouchCatchAll = require('../../Renderer/Components/touchCatchAll');
 
 class LevelDetailsOverlay {
-	textStyle = <Phaser.PhaserTextStyle>{
+
+	headStyle = <Phaser.PhaserTextStyle>{
 		fill: 'white',
 		font: 'Chewy',
-		fontSize: 22
+		fontSize: 70,
+		strokeThickness: 8,
+		boundsAlignH: "center",
+		boundsAlignV: "top"
+	}
+	smallTextStyle = <Phaser.PhaserTextStyle>{
+		fill: 'white',
+		font: 'Chewy',
+		fontSize: 22,
+		strokeThickness: 4,
+		boundsAlignH: "center",
+		boundsAlignV: "middle"
+	}
+	mediumTextStyle = <Phaser.PhaserTextStyle>{
+		fill: 'white',
+		font: 'Chewy',
+		fontSize: 28,
+		strokeThickness: 6,
+		boundsAlignH: "center",
+		boundsAlignV: "middle"
 	}
 
 	gfx: Phaser.Graphics;
 
 	closed = false;
-	
+
 	becameClosed = new LiteEvent<void>();
+
+	width = 480;
+	height = 270;
 
 	constructor(private group: Phaser.Group, private level: LevelDef, private victoryDetector: Detector, private failureDetector: Detector) {
 
 		this.addTouchCatchAll();
 		this.addBackground();
 		this.addDetails();
+		this.update();
 	}
 
 	private addTouchCatchAll() {
@@ -32,29 +56,30 @@ class LevelDetailsOverlay {
 			this.closed = true;
 			this.group.destroy();
 			this.group = null;
-			
+
 			this.becameClosed.trigger();
 		})
 	}
 
 	private addBackground() {
 		this.gfx = new Phaser.Graphics(this.group.game);
-		this.gfx.beginFill(0, 0.4);
-		this.gfx.drawRect(0, 0, 1, 1);
+		this.gfx.beginFill(0x8FB863, 1);
+		this.gfx.lineStyle(5, 0x6382B8);
+		this.gfx.drawRoundedRect(0, 0, this.width, this.height, 20);
 		this.gfx.endFill();
-		this.gfx.scale.set(this.group.game.width, this.group.game.height);
 
 		this.group.add(this.gfx);
 	}
 
 	private addDetails() {
-		this.group.add(new Phaser.Text(this.group.game, 50, 50, "Level " + this.level.levelNumber, this.textStyle));
+		this.group.add(new Phaser.Text(this.group.game, this.width / 2, 0, "Level " + this.level.levelNumber, this.headStyle).setTextBounds(0, 0, 0, 0));
 
-		this.group.add(new Phaser.Text(this.group.game, 50, 90, "Size: " + this.level.width + " x " + this.level.height, this.textStyle));
+		this.group.add(new Phaser.Text(this.group.game, this.width / 2, 100, "Size: " + this.level.width + " x " + this.level.height, this.smallTextStyle).setTextBounds(0, 0, 0, 0));
 
-		this.group.add(new Phaser.Text(this.group.game, 50, 140, this.getVictoryText() + " " + this.getFailureText(), this.textStyle));
+		this.group.add(new Phaser.Text(this.group.game, this.width / 2, 160, this.getVictoryText(), this.mediumTextStyle).setTextBounds(0, 0, 0, 0));
+		this.group.add(new Phaser.Text(this.group.game, this.width / 2, 190, this.getFailureText(), this.mediumTextStyle).setTextBounds(0, 0, 0, 0));
 
-		this.group.add(new Phaser.Text(this.group.game, 50, 200, "Click to start", this.textStyle));
+		this.group.add(new Phaser.Text(this.group.game, this.width / 2, 250, "Click to start", this.smallTextStyle).setTextBounds(0, 0, 0, 0));
 	}
 
 	private getVictoryText(): string {
@@ -70,7 +95,7 @@ class LevelDetailsOverlay {
 			return;
 		}
 
-		this.gfx.scale.set(this.group.game.width, this.group.game.height);
+		this.group.position.set((this.group.game.width - this.width) / 2, 50);
 	}
 }
 
