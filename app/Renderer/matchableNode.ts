21e259a5d54e5a4fdaf1fe02660b179480da7d46
danzel <dave@ironshod.co.nz>
2016-04-09 21:47:35 +1200

    Redo the balloverlay gfx for emojione-animals. They are less bad, not really good tho...

--- app/Renderer/matchableNode.ts
+++ app/Renderer/matchableNode.ts
@@ -18,11 +18,11 @@ class MatchableNode {
 		this.sprite = parent.create(0, 0, MatchableNode.getSpriteKey(matchable.color, matchable.type));
 
 		this.sprite.anchor = new Phaser.Point(0.5, 0.5);
-		
+
 		if (matchable.type == Type.GetToBottom) {
 			this.sprite.scale.set(0.9, 0.9);
 			parent.game.add.tween(this.sprite.scale)
-				.to({x: 1.1, y: 1.1}, 500, Phaser.Easing.Sinusoidal.InOut, true, 0, -1, true)
+				.to({ x: 1.1, y: 1.1 }, 500, Phaser.Easing.Sinusoidal.InOut, true, 0, -1, true)
 				.start();
 		}
 
@@ -110,6 +110,9 @@ class MatchableNode {
 		child.anchor.set(0.5, 0.5);
 		this.sprite.addChild(child);
 		this.overlay = child;
+
+		child.game.add.tween(child.scale)
+			.to({ x: 0.95, y: 0.95 }, 1000, Phaser.Easing.Sinusoidal.InOut, true, 0, -1, true)
 	}
 
 	updateForTransformed() {
