6f55ec055266217cd1ec6baf00b745879b6c95b2
David Leaver <danzel@localhost.geek.nz>
2016-05-31 21:11:02 +1200

    Make enter on the name input field work. Need to test on IE/Firefox.

--- app/Scenes/WelcomeScreen/welcomeScreen.ts
+++ app/Scenes/WelcomeScreen/welcomeScreen.ts
@@ -6,7 +6,6 @@ require('./welcomeScreen.css');
 class WelcomeScreen {
 	private element: HTMLElement;
 	private nickname: HTMLInputElement;
-	private button: HTMLInputElement;
 	private hidenames: HTMLInputElement;
 
 	onLogin: (nickname: string, hideNames: boolean) => void;
@@ -21,14 +20,22 @@ class WelcomeScreen {
 	}
 
 	private addEventListeners() {
+		
 		this.nickname = <HTMLInputElement>this.element.getElementsByClassName('nickname')[0];
-		this.button = <HTMLInputElement>this.element.getElementsByClassName('button')[0];
 		this.hidenames = <HTMLInputElement>this.element.getElementsByClassName('hidenames')[0];
 
-		this.button.addEventListener('click', () => {
+		let form = <HTMLFormElement>document.getElementById('login-form');
+		let button = <HTMLInputElement>this.element.getElementsByClassName('button')[0];
+
+		let buttonAction = (ev: Event) => {
 			this.element.style.display = 'none';
 			this.onLogin(this.nickname.value, this.hidenames.checked);
-		})
+
+			ev.preventDefault();
+			return false;
+		};
+		form.addEventListener('submit', buttonAction);
+		button.addEventListener('click', buttonAction)
 	}
 }
 
