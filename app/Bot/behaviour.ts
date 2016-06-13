4214c65dc6d1e5c81ebda8222309ae0719640e54
danzel <dave@ironshod.co.nz>
2016-06-13 21:43:52 +1200

    Maybe the last dumb bot bug

--- app/Bot/behaviour.ts
+++ app/Bot/behaviour.ts
@@ -1,5 +1,6 @@
 import BotHelper = require('./botHelper');
 import InputApplier = require('../Simulation/inputApplier');
+import MagicNumbers = require('../Simulation/magicNumbers');
 import Move = require('./move');
 import Simulation = require('../Simulation/simulation');
 
@@ -14,7 +15,7 @@ abstract class Behaviour {
 
 	performMove(move: Move): void {
 		let left = this.simulation.grid.findMatchableAtPosition(move.x, move.y);
-		let right = this.simulation.grid.findMatchableAtPosition(move.x + move.direction.x, move.y + move.direction.y);
+		let right = this.simulation.grid.findMatchableAtPosition(move.x + move.direction.x, move.y + move.direction.y * MagicNumbers.matchableYScale);
 		this.inputApplier.swapMatchable(left, right, move.direction)
 
 		this.lastPos.x = move.x;
