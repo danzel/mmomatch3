56fbe44923479ff79d2fb53d8842b21e8bfac82a
danzel <danzel@localhost.geek.nz>
2016-10-27 22:08:30 +1300

    Fix showing the big ad when we should have been showing the small one. Position the bit one correctly too (off by 4px... bad at math...)

--- app/HtmlOverlay/bannerAdManager.ts
+++ app/HtmlOverlay/bannerAdManager.ts
@@ -8,7 +8,7 @@ class BannerAdManager {
 	show() {
 		if (this.element) { //May be totally adblocked off the screen
 			if (this.element.innerHTML == '') {
-				if (window.innerWidth <= 400) {
+				if (window.innerWidth < 728) {
 					this.element.innerHTML = '<ins class="adsbygoogle" style="display:inline-block;width:320px;height:100px" data-ad-client="ca-pub-4749031612968477" data-ad-slot="7034687548"></ins>';
 				} else {
 					this.element.innerHTML = '<ins class="adsbygoogle" style="display:inline-block;width:728px;height:90px" data-ad-client="ca-pub-4749031612968477" data-ad-slot="9178345940"></ins>';
