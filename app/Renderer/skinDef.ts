5754292b92630dbc9554f93dc04d556ea7900bfd
danzel <danzel@localhost.geek.nz>
2017-03-11 15:23:40 +1300

    Add Kenney skin for the Matches level (since it is least liked level type at 0.66)

--- app/Renderer/skinDef.ts
+++ app/Renderer/skinDef.ts
@@ -6,12 +6,15 @@ class SkinDef {
 
 	private static lianne = new SkinDef('skin-lianne-animals', '#1c3d01')
 	private static default = new SkinDef('skin-emojione-animals', '#273348');
+	private static kenney = new SkinDef('skin-kenney-animals', '#2f014f');
 
 	static getForLevel(level: LevelDef): SkinDef {
 		if (level.victoryType == VictoryType.GrowOverGrid) {
 			return SkinDef.lianne;
 		}
-
+		if (level.victoryType == VictoryType.Matches) {
+			return SkinDef.kenney;
+		}
 		return SkinDef.default;
 	}
 }
