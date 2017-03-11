c70aa5b978c2f11f372f22a9ca193d0feb6b1639
danzel <danzel@localhost.geek.nz>
2017-03-11 16:41:22 +1300

    New particle sprites. Change their animations a bit.

--- app/Renderer/simulationParticleRenderer.ts
+++ app/Renderer/simulationParticleRenderer.ts
@@ -12,18 +12,17 @@ class SimulationParticleRenderer {
 		parentGroup.add(this.emitter);
 
 		this.emitter.gravity = 1000;
-		this.emitter.setAlpha(1, 0, 6000, Phaser.Easing.Cubic.Out);
+		this.emitter.setAlpha(0.9, 0, 4000, Phaser.Easing.Cubic.In);
 		this.emitter.setXSpeed(-300, 300);
 		this.emitter.setYSpeed(-700, -100);
 		this.emitter.setRotation(-60, 60);
 		this.emitter.setSize(100, 100);
+		this.emitter.setScale(1, 0, 1, 0, 4000, Phaser.Easing.Cubic.In);
 	}
 
 	simulationChanged(simulation: Simulation, playerId: number, currentSkin: string) {
 		simulation.comboOwnership.ownedMatchPerformed.on(m => {
-			if (m.players.indexOf(playerId) >= 0) {
-				this.ownedMatchPerformed(m)
-			}
+			this.ownedMatchPerformed(m, m.players.indexOf(playerId) >= 0);
 		});
 
 		if (this.currentSkin != currentSkin) {
@@ -40,12 +39,12 @@ class SimulationParticleRenderer {
 		}
 	}
 
-	private ownedMatchPerformed(match: OwnedMatch): void {
+	private ownedMatchPerformed(match: OwnedMatch, ourMatch: boolean): void {
 		match.match.matchables.forEach(m => {
 			this.emitter.x = (m.x + 0.5) * MatchableRenderer.PositionScalar;
 			this.emitter.y = -(m.y / MagicNumbers.matchableYScale + 0.5) * MatchableRenderer.PositionScalar;
 
-			this.emitter.start(true, 2000, null, 10);
+			this.emitter.start(true, 2000, null, ourMatch ? 6 : 2);
 		})
 	}
 
